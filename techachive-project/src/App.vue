3bbde7a0a0717a91dda214b6f65899e836d6a438
Your Name <ragheb.d21@htlwienwest.at>
2025-02-25 20:32:26 +0100

    commit_homepage

--- techachive-project/src/App.vue
+++ techachive-project/src/App.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+// Importiere die HelloWorld-Komponente
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, // Registrierung der HelloWorld-Komponente
+  },
+};
 </script>
 
-<template>
-  <HelloWorld message="Willkommen bei TechAchieve!" />
-</template>
+<style>
+/* Hier kannst du globale Styles hinzufügen, falls gewünscht */
+#app {
+  text-align: center;
+}
+</style>
