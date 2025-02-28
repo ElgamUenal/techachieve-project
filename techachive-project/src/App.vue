3bced6343cd69967a50f37c685a0e43a32b63053
Your Name <ragheb.d21@htlwienwest.at>
2025-02-28 16:07:09 +0100

    commit_app

--- techachive-project/src/App.vue
+++ techachive-project/src/App.vue
@@ -6,7 +6,7 @@
         <p class="website_name">LOGO</p>
         <div class="menu_links">
           <router-link to="/" class="link">Home</router-link>
-          <router-link to="/projects" class="link">Nachhilfe</router-link>
+          <router-link to="/Nachhilfe" class="link">Nachhilfe</router-link>
           <router-link to="/mitschriften" class="link">Mitschriften</router-link>
           <router-link to="/contacts" class="link">About</router-link>
         </div>
