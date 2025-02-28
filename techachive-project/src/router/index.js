644afa0d528aa548153c1b098b9fc9ce287b9122
ElgamUenal <uenal.e21@htlwienwest.at>
2025-02-28 16:09:04 +0100

    Router changed

--- techachive-project/src/router/index.js
+++ techachive-project/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
 import Mitschriften from '@/components/mitschriften.vue';
+import Nachhilfe from '@/components/Nachhilfe.vue';
 
 const routes = [
   { path: '/', component: HelloWorld }, // HelloWorld wird nur für die Startseite angezeigt
   { path: '/mitschriften', component: Mitschriften },
+  {path: '/nachhilfe', component: Nachhilfe }
 ];
 
 const router = createRouter({
